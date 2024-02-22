02383d6a753929608adf20de0f2ada7e22e9f4ae
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-02-22 16:13:58 +0000

    Complete extensions

--- src/main/java/com/example/task_01/controllers/GreetingController.java
+++ src/main/java/com/example/task_01/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.task_01.controllers;
 
+import com.example.task_01.models.Celebration;
 import com.example.task_01.models.Greeting;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,5 +12,11 @@ public class GreetingController {
     public String newGreeting(@RequestParam String time){
         Greeting greeting = new Greeting("Colin", "afternoon");
         return String.format("Good %s %s!", time, greeting.getName());
+    }
+
+    @GetMapping(value = "/eid")
+    public String newCelebration(){
+        Celebration celebration = new Celebration("Happy");
+        return String.format("%s eid", celebration.getMessage());
     }
 }
